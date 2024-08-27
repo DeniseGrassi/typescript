9e34166000e4079cf45e514c044d9b2d0a3670ad
Denise Grassi <denygrassi_@hotmail.com>
2024-08-27 13:33:05 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(a, b) {
+    return a * b;
+}
+const resultadoMultiplicacao = multiplicar(20, 20);
+console.log(resultadoMultiplicacao);
+// Função de saudação
+function saudacao(nome) {
+    return "Olá, " + nome; //concatenacao
+}
+const mensagemSaudacao = saudacao("Denise");
+console.log(mensagemSaudacao);
---
+++ main.ts
@@ -0,0 +1,19 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+
+const resultadoMultiplicacao = multiplicar(20, 20); 
+console.log(resultadoMultiplicacao);
+
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return "Olá, " + nome;  //concatenacao
+}
+
+
+const mensagemSaudacao = saudacao("Denise"); 
+console.log(mensagemSaudacao);
+
